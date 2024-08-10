a73aa672a20efa58166d36a907a8a3935b5bc343
Jam Nguyen <jamnguyen@senecaegs.com>
2024-08-10 08:39:30 +0700

    update: data generator

---
+++ src/data_generation/__main__.py
@@ -0,0 +1,67 @@
+import os
+import cv2
+import glob
+import random
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from raindrop.data_generation.gen_albumentation import GenWithAlbumentation
+from raindrop.data_generation.get_position_matrix import get_position_matrix
+
+
+def composition_img(img,alpha,position_matrix,length=2):
+    h, w = img.shape[0:2]
+    dis_img = img.copy()
+
+    for x in range(h):
+        for y in range(w):
+            u,v = int(position_matrix[0,x,y]/length),int(position_matrix[1,x,y]/length)
+            if (u != 0 and v != 0):
+                if((u<h) and (v<w)):
+                    dis_img [x,y,:] = dis_img[u,v,:]
+                elif(u<h):
+                    print(w)
+                    dis_img[x, y, :] = dis_img[u, np.random.randint(0,w-1), :]
+                elif(v<w):
+                    print(v)
+                    dis_img[x, y, :] = dis_img[np.random.randint(0,h-1), v, :]
+
+    dis_img = cv2.blur(dis_img,(3,3))*(0.9)
+
+    img = (alpha/255)*dis_img + (1-(alpha/255))*img
+    img = np.array(img,dtype=np.uint8)
+    return img
+
+
+if __name__ == "__main__":
+
+    # Change this to the folder of cityscape images.
+    texture_save_dir = 'Honda-data/gen_raindrop/alpha_textures/texture'
+    alpha_save_dir = 'Honda-data/gen_raindrop/alpha_textures/alpha'
+    gen_raindrop = 'Honda-data/cityscapes-leftimg8bit-trainvaltest/20240808_generated_data/valid'
+    original_dir = 'Honda-data/cityscapes-leftimg8bit-trainvaltest/original_data/valid'
+    os.makedirs(gen_raindrop, exist_ok=True)
+    alpha_imgs = os.listdir(alpha_save_dir)
+
+    for img_path in tqdm(glob.glob(os.path.join(original_dir, "*"))):
+        basename = os.path.splitext(os.path.basename(img_path))[0]
+        img = cv2.imread(img_path)
+        if np.random.uniform() > 0.5:
+            alpha_img_name = alpha_imgs[random.randint(0,len(alpha_imgs)-1)]
+            texture_img_name = 'texture_' + alpha_img_name
+            
+            alpha_img = cv2.imread(os.path.join(alpha_save_dir,alpha_img_name))
+            texture_img = cv2.imread(os.path.join(texture_save_dir,texture_img_name))
+            
+            position_matrix, alpha = get_position_matrix(texture_img,alpha_img,img.shape[0:2],img)
+            if np.random.uniform() > 0.5:
+                img = GenWithAlbumentation._with_GlassBlur()(image=img)['image']
+            img = composition_img(img,alpha,position_matrix)
+            prefix = 'raindrop'
+        else:
+            prefix, img = GenWithAlbumentation.compose_transformation(img)
+        
+        prefix_folder = os.path.join(gen_raindrop, prefix)
+        os.makedirs(prefix_folder, exist_ok=True)
+        cv2.imwrite(os.path.join(prefix_folder, f'{basename}.png'), img)
---
+++ src/data_generation/gen_albumentation.py
@@ -0,0 +1,143 @@
+import albumentations as A
+import numpy as np
+
+class GenWithAlbumentation:
+
+    @classmethod
+    def _with_random_rain(cls):
+        transform = A.RandomRain(
+            slant_lower=-5,  # int | None
+            slant_upper=5,  # int | None
+            drop_length=10,  # int
+            drop_width=1,  # int
+            drop_color=(200, 200, 200),  # tuple[int, int, int]
+            blur_value=4,  # int
+            brightness_coefficient=0.7,  # float
+            rain_type=None,  # RainMode | None
+            always_apply=None,  # bool | None
+            p=1.0,  # float
+        )
+        return transform
+    
+    @classmethod
+    def _with_random_fog(cls):
+        transform = A.RandomFog(
+            fog_coef_lower=0.3,  # float | None
+            fog_coef_upper=0.5,  # float | None
+            alpha_coef=0.1,  # float
+            always_apply=None,  # bool | None
+            p=1.0,  # float
+        )
+        
+        return transform
+    
+    @classmethod
+    def _with_random_SunFlare(cls):
+        transform = A.RandomSunFlare(
+            flare_roi=(0, 0, 1, 0.5),  # tuple[float, float, float, float]
+            angle_lower=0,  # float | None
+            angle_upper=1,  # float | None
+            num_flare_circles_lower=10,  # int | None
+            num_flare_circles_upper=20,  # int | None
+            src_radius=200,  # int
+            src_color=(255, 255, 255),  # tuple[int, ...]
+            always_apply=None,  # bool | None
+            p=1.0,  # float
+        )
+        
+        return transform
+    
+    @classmethod
+    def _with_random_BrightnessContrast(cls):
+        transform = A.RandomBrightnessContrast(
+            brightness_limit=(-0.2, -0.1),  # ScaleFloatType
+            contrast_limit=(-0.2, 0.2),  # ScaleFloatType
+            brightness_by_max=True,  # bool
+            always_apply=None,  # bool | None
+            p=1.0,  # float
+        )
+        return transform
+    
+    @classmethod
+    def _with_PixelDropout(cls):
+        transform = A.PixelDropout(
+            dropout_prob=0.01,  # float
+            per_channel=False,  # bool
+            drop_value=0,  # ScaleFloatType | None
+            mask_drop_value=None,  # ScaleFloatType | None
+            always_apply=None,  # bool | None
+            p=1.0,  # float
+        )
+        
+        return transform
+    
+    @classmethod
+    def _with_GlassBlur(cls):
+        transform = A.GlassBlur(
+            sigma=0.5,  # float
+            max_delta=2,  # int
+            iterations=2,  # int
+            mode="fast",  # Literal['fast', 'exact']
+            always_apply=None,  # bool | None
+            p=1.0,  # float
+        )
+        return transform
+    
+    @classmethod
+    def _with_Blur(cls):
+        transform = A.OneOf([
+            A.MotionBlur(
+                blur_limit=9,  # ScaleIntType
+                allow_shifted=True,  # bool
+                always_apply=None,  # bool | None
+                p=1.0,  # float
+            ),
+            A.GlassBlur(
+                sigma=0.2,  # float
+                max_delta=2,  # int
+                iterations=2,  # int
+                mode="fast",  # Literal['fast', 'exact']
+                always_apply=None,  # bool | None
+                p=1.0,  # float
+            ),
+            A.Blur(
+                blur_limit=7,  # ScaleIntType
+                p=1.0,  # float
+                always_apply=None,  # bool | None
+            ),
+            A.GaussianBlur(
+                blur_limit=(5, 7),  # ScaleIntType
+                sigma_limit=0,  # ScaleFloatType
+                always_apply=None,  # bool | None
+                p=1.0,  # float
+            ),
+            A.ZoomBlur(
+                max_factor=(1, 1.09),  # ScaleFloatType
+                step_factor=(0.01, 0.02),  # ScaleFloatType
+                always_apply=None,  # bool | None
+                p=1.0,  # float
+            )
+        ], p=1)
+        
+        return transform
+    
+    @classmethod
+    def _with_Posterize(cls):
+        transform = A.Posterize(
+            num_bits=4,  # int | tuple[int, int] | tuple[int, int, int]
+            always_apply=None,  # bool | None
+            p=1.0,  # float
+        )
+        return transform
+    
+    @classmethod
+    def compose_transformation(cls, image):
+        transforms = {
+            'random_rain': cls._with_random_rain(),
+            'random_fog': cls._with_random_fog(),
+            'random_brightness_contrast': cls._with_random_BrightnessContrast()
+        }
+        aug = np.random.choice(list(transforms.keys()))
+        transform = transforms[aug]
+        transformed = transform(image=image)
+        return aug, transformed['image']
